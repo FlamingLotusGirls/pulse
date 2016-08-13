b7469d6d3c2e22125a8564001b7489e033a93d94
Bob Kerns <rwk@acm.org>
2016-08-13 14:44:31 -0700

    Updated for new Dev Studio, and downgrade to API 19.

--- android/PulseController/app/src/main/java/org/flg/hiromi/pulsecontroller/MainActivity.java
+++ android/PulseController/app/src/main/java/org/flg/hiromi/pulsecontroller/MainActivity.java
@@ -1,18 +1,21 @@
 package org.flg.hiromi.pulsecontroller;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
-import java.net.URL;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import org.json.*;
+import java.net.URL;
 
 public class MainActivity extends ActionBarActivity {
 
