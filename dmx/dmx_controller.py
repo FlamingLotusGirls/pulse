a58ca04145b0e4404b5a0604232e00674ed27b5a
pewing <philip.jh.ewing@gmail.com>
2016-08-12 18:40:49 -0700

    figured out separating lights, and have several heartbeat versions

--- dmx/dmx_controller.py
+++ dmx/dmx_controller.py
@@ -20,11 +20,11 @@ MULTICAST_TTL  = 4
 BAUDRATE       = 19200
 
 DMX_ADDRESS = "/dev/tty.usbserial-EN195017"
-# White lights if all three channels on 255 at once
-DMX_GREEN_CHANNEL = 3
 DMX_RED_CHANNEL = 2
+DMX_GREEN_CHANNEL = 3
 DMX_BLUE_CHANNEL = 4
 DMX_WHITE_CHANNEL = 5
+DMX_CHANNEL_COUNT = 6 # Can be 6/7/8/12
 
 running = True
 allowHeartBeats = True
@@ -79,7 +79,6 @@ def handleHeartBeatData(heartBeatData):
     print pod_id
     print currentHeartBeatSource
     if pod_id is currentHeartBeatSource and allowHeartBeats:
-        print "after if"
         print "sequenceID", sequenceId
         print "beatIntervalMs", beatIntervalMs
         print "beatOffsetMs", beatOffsetMs
@@ -174,18 +173,100 @@ def renderEvents():
 
     if currentEvents:
         if not dmx:
-            dmx = pysimpledmx.DMXConnection(DMX_ADDRESS)
+            pass
+            # dmx = pysimpledmx.DMXConnection(DMX_ADDRESS)
         for event in currentEvents:
             heartbeat(event)
 
 def heartbeat(event):
+    # pattern of a heartbeat divided in 10
+    # 1: flat
+    # 2: small bump
+    # 3: flat, then small down
+    # 4: big up, then bigger down
+    # 5: back to normal, then flat
+    # 6: small (slightly larger) bump
+    # 7: flat
+    #
+    # flat: 100
+    # 1: 100
+    # 2: 100->120->100
+    # 3: 100->100->80
+    # 4: 80->250->60
+    # 5: 60->100->100
+    # 6: 100->130->100
+    # 7: 100
+
+    # TODO: Ask carolyn about heartbeat... they seem to discard information about frequency
+
     print "event ", event
-    timeWindow = datetime.datetime.now() + datetime.timedelta(milliseconds = 5)
+
     now = datetime.datetime.now()
+    timeWindow = now + datetime.timedelta(milliseconds = 1000)
+    seventh = (1000//7)/1000.0
+
     dmx.setChannel(DMX_RED_CHANNEL, 100)
+    dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 100)
+    dmx.render()
+    time.sleep(seventh + (seventh / 3))
+    dmx.setChannel(DMX_RED_CHANNEL, 120)
+    dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 120)
+    dmx.render()
+    time.sleep(seventh/3)
+    dmx.setChannel(DMX_RED_CHANNEL, 100)
+    dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 100)
+    dmx.render()
+    time.sleep(seventh)
+    dmx.setChannel(DMX_RED_CHANNEL, 80)
+    dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 80)
+    dmx.render()
+    time.sleep((seventh/3)*2)
+    dmx.setChannel(DMX_RED_CHANNEL, 200)
+    dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 250)
+    dmx.render()
+    time.sleep(seventh/3)
+    dmx.setChannel(DMX_RED_CHANNEL, 60)
+    dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 60)
+    dmx.render()
+    time.sleep((seventh/3)*2)
+    dmx.setChannel(DMX_RED_CHANNEL, 100)
+    dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 100)
+    dmx.render()
+    time.sleep(seventh)
+    dmx.setChannel(DMX_RED_CHANNEL, 130)
+    dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 130)
+    dmx.render()
+    time.sleep(seventh/3)
+    dmx.setChannel(DMX_RED_CHANNEL, 100)
+    dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 100)
+    dmx.render()
+    time.sleep((seventh/3)*2)
+
+
+
+    # while now < timeWindow:
+    #     if (now + datetime.timedelta(milliseconds = seventh)) < timeWindow:
+    #         dmx.setChannel(DMX_RED_CHANNEL, 100)
+    #         dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 100)
+    #         dmx.render()
+
+
+
+
+    now = datetime.datetime.now()
+
+
+    dmx.setChannel(DMX_RED_CHANNEL, 100)
+    dmx.setChannel(DMX_GREEN_CHANNEL+12, 200)
     dmx.render()
     time.sleep(0.2)
-    for i in range(75):
+    for i in range(20):
+        dmx.setChannel(DMX_RED_CHANNEL, 100 - i)
+        dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 100-i)
+        dmx.render()
+        time.sleep(0.001)
+    dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 100)
+    for i in range(85):
         dmx.setChannel(DMX_RED_CHANNEL, 100 + i*2)
         dmx.render()
         time.sleep(0.002)
@@ -194,50 +275,93 @@ def heartbeat(event):
         dmx.render()
         time.sleep(0.001)
     for i in range(75):
+        dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 100 + i*2)
+        dmx.render()
+        time.sleep(0.002)
+    for i in range(100):
+        dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 200 - i)
+        dmx.render()
+        time.sleep(0.001)
+    print "difference1 = ", (datetime.datetime.now() - now)
+    time.sleep(1)
+
+    for i in range(20):
+        dmx.setChannel(DMX_RED_CHANNEL, 100 - i)
+        dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 100-i)
+        dmx.render()
+        time.sleep(0.001)
+    dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 100)
+    for i in range(85):
         dmx.setChannel(DMX_RED_CHANNEL, 100 + i*2)
         dmx.render()
         time.sleep(0.002)
     for i in range(100):
         dmx.setChannel(DMX_RED_CHANNEL, 200 - i)
+        dmx.render()
+        time.sleep(0.001)
+    for i in range(75):
+        dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 100 + i*2)
+        dmx.render()
+        time.sleep(0.002)
+    for i in range(100):
+        dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 200 - i)
         dmx.render()
         time.sleep(0.001)
     time.sleep(1)
 
-    for i in range(75):
+    for i in range(20):
+        dmx.setChannel(DMX_RED_CHANNEL, 100 - i)
+        dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 100 - i)
+        dmx.render()
+        time.sleep(0.001)
+    for i in range(85):
         dmx.setChannel(DMX_RED_CHANNEL, 100 + i*2)
+        dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 100 + i*2)
         dmx.render()
         time.sleep(0.002)
     for i in range(100):
         dmx.setChannel(DMX_RED_CHANNEL, 200 - i)
+        dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 200 - i)
         dmx.render()
         time.sleep(0.001)
     for i in range(75):
         dmx.setChannel(DMX_RED_CHANNEL, 100 + i*2)
+        dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 100 + i*2)
         dmx.render()
         time.sleep(0.002)
     for i in range(100):
         dmx.setChannel(DMX_RED_CHANNEL, 200 - i)
-        dmx.render()
-        time.sleep(0.001)
-    time.sleep(1)
-
-    for i in range(75):
-        dmx.setChannel(DMX_RED_CHANNEL, 100 + i*2)
-        dmx.render()
-        time.sleep(0.002)
-    for i in range(100):
-        dmx.setChannel(DMX_RED_CHANNEL, 200 - i)
-        dmx.render()
-        time.sleep(0.001)
-    for i in range(75):
-        dmx.setChannel(DMX_RED_CHANNEL, 100 + i*2)
-        dmx.render()
-        time.sleep(0.002)
-    for i in range(100):
-        dmx.setChannel(DMX_RED_CHANNEL, 200 - i)
+        dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 200 - i)
         dmx.render()
         time.sleep(0.001)
     time.sleep(1)
+    now = datetime.datetime.now()
+    for i in range(20):
+        dmx.setChannel(DMX_RED_CHANNEL, 100 - i)
+        dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 100 - i)
+        dmx.render()
+        time.sleep(0.001)
+    for i in range(85):
+        dmx.setChannel(DMX_RED_CHANNEL, 100 + i*2)
+        dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 100 + i*2)
+        dmx.render()
+        time.sleep(0.002)
+    for i in range(100):
+        dmx.setChannel(DMX_RED_CHANNEL, 200 - i)
+        dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 200 - i)
+        dmx.render()
+        time.sleep(0.001)
+    for i in range(75):
+        dmx.setChannel(DMX_RED_CHANNEL, 100 + i*2)
+        dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 100 + i*2)
+        dmx.render()
+        time.sleep(0.002)
+    for i in range(100):
+        dmx.setChannel(DMX_RED_CHANNEL, 200 - i)
+        dmx.setChannel(DMX_RED_CHANNEL + DMX_CHANNEL_COUNT, 200 - i)
+        dmx.render()
+        time.sleep(0.001)
+    print "difference2 = ", (datetime.datetime.now() - now)
 
     sys.exit()
 
@@ -278,6 +402,11 @@ if __name__ == '__main__':
     heartBeatListener = createBroadcastListener(HEARTBEAT_PORT)
     commandListener   = createBroadcastListener(COMMAND_PORT)
     eventQueue = []
+    print datetime.datetime.now()
+    print datetime.datetime.now()
+    print datetime.datetime.now()
+    print datetime.datetime.now()
+
 
     try:
         while (running):
